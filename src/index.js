a85bec476d357a1106936baca7a7120667358982
MichalGomulczak <michal.gomulczak1@gmail.com>
2025-04-27 03:01:32 +0200

    working - landing left

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ const authRouter = require('../routes/auth');
 const descRouter = require('../routes/desc');
 const userRouter = require('../routes/user');
 const cors = require('cors');
+const midnightCheck = require('../cron/midnightCheck');
+const cron = require('node-cron');
+
 const app = express();
 const db = require('../database/db');
+
 
 app.use(cors({
   origin: 'https://denchal.github.io',
@@ -19,3 +23,10 @@ app.use('/user', userRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+cron.schedule('0 0 * * *', () => {
+  midnightCheck();
+  console.log('Wykonuję zadanie aktualizację limitów');
+});
+
+midnightCheck();
